31124276fcfc29a036c3d10905ca1b72443bf7e1
Kate Johnson <johnson.kat.g@gmail.com>
2022-03-06 00:05:28 -0500

    bug fixes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,8 +5,6 @@ const withAuth = require('../../utils/auth');
 
 // get all posts
 router.get('/', (req, res) => {
-<<<<<<< HEAD:routes/api/post-routes.js
-  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -41,48 +39,6 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-=======
-    Post.findAll({
-        attributes: [
-            'id',
-            'post_url',
-            'title',
-            'created_at',
-            [
-                sequelize.literal(
-                    '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
-                ),
-                'vote_count'
-            ]
-        ],
-        order: [['created_at', 'DESC']],
-        include: [
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'comment_text',
-                    'post_id',
-                    'user_id',
-                    'created_at'
-                ],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-        .then((dbPostData) => res.json(dbPostData))
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
->>>>>>> develop:controllers/api/post-routes.js
 });
 // get a single post
 router.get('/:id', (req, res) => {
@@ -130,13 +86,11 @@ router.get('/:id', (req, res) => {
     });
 });
 // create a post
-<<<<<<< HEAD:routes/api/post-routes.js
-router.post('/', (req, res) => {
-  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+router.post('/', withAuth, (req, res) => {
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
-    user_id: req.body.user_id
+    user_id: req.session.user_id
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -145,17 +99,22 @@ router.post('/', (req, res) => {
     });
 });
 // put a vote on a post
-router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then((updatedPostData) => res.json(updatedPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+router.put('/upvote', withAuth, (req, res) => {
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote(
+      { ...req.body, user_id: req.session.user_id },
+      { Vote, Comment, User }
+    )
+      .then((updatedVoteData) => res.json(updatedVoteData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 // update a post title
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title
@@ -179,7 +138,7 @@ router.put('/:id', (req, res) => {
     });
 });
 // delete a post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
   Post.destroy({
     where: {
       id: req.params.id
@@ -191,62 +150,6 @@ router.delete('/:id', (req, res) => {
         return;
       }
       res.json(dbPostData);
-=======
-router.post('/', withAuth, (req, res) => {
-    Post.create({
-        title: req.body.title,
-        post_url: req.body.post_url,
-        user_id: req.session.user_id
-    })
-        .then((dbPostData) => res.json(dbPostData))
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-// put a vote on a post
-router.put('/upvote', withAuth, (req, res) => {
-    if (req.session) {
-      // pass session id along with all destructured properties on req.body
-        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-            .then(updatedVoteData => res.json(updatedVoteData))
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json(err);
-        })
-    }
-});
-// update a post title
-router.put('/:id', withAuth, (req, res) => {
-    Post.update(
-        {
-            title: req.body.title
-        },
-        {
-            where: {
-                id: req.params.id
-            }
-        }
-    )
-        .then((dbPostData) => {
-            if (!dbPostData) {
-                res.status(404).json({ message: 'No post found with this id' });
-                return;
-            }
-            res.json(dbPostData);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-// delete a post
-router.delete('/:id', withAuth, (req, res) => {
-    Post.destroy({
-        where: {
-            id: req.params.id
-        }
->>>>>>> develop:controllers/api/post-routes.js
     })
     .catch((err) => {
       console.log(err);
